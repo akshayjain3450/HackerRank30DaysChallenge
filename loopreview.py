d673a409ad60f7cdc300c0e1e46c81a04d18478f
akshayjain3450 <akshayjain3450@gmail.com>
2019-11-10 09:45:39 +0530

    Let's Review

---
+++ loopreview.py
@@ -0,0 +1,16 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+T = int(input())
+if T < 1 and T > 10:
+    exit()
+else:
+    for i in range(T):
+        S = input()
+        odd = ''
+        even = ''
+        for j in range(len(S)):
+            if j % 2 == 0:
+                odd = odd + S[j]
+            else:
+                even = even + S[j]
+        print(odd,even)
